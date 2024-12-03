c989c39b18c41a2ded88e948e08e878465d90be7
Antoine Lochet <antoine.2.lochet@eviden.com>
2024-12-03 09:57:08 +0100

    Fixed mock function feature parameters reading Compile with C++14

--- src/lib/crypto/OSSLCryptoFactory.h
+++ src/lib/crypto/OSSLCryptoFactory.h
@@ -86,7 +86,7 @@ private:
 	OSSLCryptoFactory();
 
 	// The one-and-only instance
-#ifdef HAVE_CXX11
+#ifdef HAVE_CXX14
 	static std::unique_ptr<OSSLCryptoFactory> instance;
 #else
 	static std::auto_ptr<OSSLCryptoFactory> instance;
